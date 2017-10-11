2244a390699ee14223e4520e755eb19fcf341003
Samuel Mensah <samuelnmensah@live.com>
2017-10-11 11:36:41 -0700

    added new files

---
+++ DotNetTracing/TraceOne/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraceOne
+{
+    class Program
+    {
+        /// <summary>
+        /// //Writing Debug and Trace Statements to file via code configuration
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            
+
+            // Create a file for output named TestFile.txt.
+            Stream myFile = File.Create("TestFile.txt");
+
+            /* Create a new text writer using the output stream, and add it to
+             * the trace listeners. */
+            TextWriterTraceListener myTextListener = new TextWriterTraceListener(myFile);
+
+            //Add listener to collection
+            Trace.Listeners.Add(myTextListener);
+            Debug.Listeners.Add(myTextListener);
+
+            // Write output to the file.
+            Trace.Write("Test output ");
+            Debug.Write("Trace 1 with Debug");
+
+            // Flush the output.
+            Trace.Flush();
+            Trace.Close();
+
+
+
+        }
+    }
+}
